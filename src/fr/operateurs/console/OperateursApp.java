ab114c6996ab2677c0112441859b8aa0a5fbaadf
poloch0n <50014021+poloch0n@users.noreply.github.com>
2019-05-02 14:14:57 +0200

    commentary

--- src/fr/operateurs/console/OperateursApp.java
+++ src/fr/operateurs/console/OperateursApp.java
@@ -9,25 +9,28 @@ public class OperateursApp {
 		// Affichage du premier message
 		System.out.println("*****  Application Op�rateurs *****\r\n" + 
 				"Veuillez saisir le premier nombre�");
-		// R�cup�ration du premier nombre taper
+		// Récupération du premier nombre tapé
 		
 		float firstNumber = getNumber();
 		// Affichage du deuxieme message
 		System.out.println("Veuillez saisir le second nombre�");
 
 		float secondNumber = getNumber();
+		// Affiche la phrase pour chaque opération
 		showOperation(firstNumber,secondNumber,"+");
-
 		showOperation(firstNumber,secondNumber,"-");
 		showOperation(firstNumber,secondNumber,"*");
 		showOperation(firstNumber,secondNumber,"/");
 		showOperation(firstNumber,secondNumber,"%");
 	}
 	
+	// Fonction permettant d'afficher une phrase pour une opération
 	private static void showOperation(float firstNumber, float secondNumber, String string) {
 
 		float result = 0 ;
 		String message = "";
+		
+		// Distinction des cas pour chaque opération
 		switch(string) {
 		case "+":
 			result = firstNumber + secondNumber;
@@ -42,32 +45,39 @@ public class OperateursApp {
 			if(secondNumber != 0) {
 				result = firstNumber / secondNumber;
 			} else {
+				// Distinction extreme de la division par zéro
 				message = " \r\n you tried to divise by 0 ? really ? nice try";
 			}
-			
 		      break;
 		case "%":
 			if(secondNumber != 0) {
 				result = firstNumber % secondNumber;
 			} else {
+				// Distinction extreme du calcul du modulo par 0
 				message = " \r\n you tried to get the modulo from 0 ? you either are testing me or pretty d... curious :p";
 			}
 		      break;
-		default: System.out.println("error intern, contact dev");
+		default: 
+			// Cas impossible sauf évolution du code
+			System.out.println("contact Dev : scope 'showOperation'");
 		}
-		//show message
+		
+		//Affichage du message
 		if(message == "") {
 			System.out.println(firstNumber + " " + string + " " + secondNumber + " = " + result);
 		} else {
+			// Affichage des cas extremes
 			System.out.println(message);
 		};
 	}
 
+	// Récupération du nombre tapé
 	private static float getNumber() {
 		questionUser = new Scanner(System.in);
 		try {
 			return questionUser.nextFloat();
 		} catch(Exception e)  {
+			// Gestion des cas où l'utilisateur ne rentre pas un nombre
 			float random =  (float)(int) (Math.random()*100 +1);
 			System.out.println("\r\n you didn't enter a number, we choose "+random+" for you");
 			return random;
