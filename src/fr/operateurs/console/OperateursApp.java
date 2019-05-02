b4ce47d9348eaed91b14d57ddb9169efa76fca43
poloch0n <50014021+poloch0n@users.noreply.github.com>
2019-05-02 12:43:55 +0200

    init project

---
+++ src/fr/operateurs/console/OperateursApp.java
@@ -0,0 +1,7 @@
+package fr.operateurs.console;
+
+public class OperateursApp {
+	public static void main(String [] args) {
+		System.out.println("Hellow World");
+	}
+}
---
+++ src/fr/operateurs/console/package-info.java
@@ -0,0 +1,1 @@
+package fr.operateurs.console;
