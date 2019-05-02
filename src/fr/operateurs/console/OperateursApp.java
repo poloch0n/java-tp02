953f52094444adb55835800a62c421fb9021d6c5
poloch0n <50014021+poloch0n@users.noreply.github.com>
2019-05-02 13:09:27 +0200

    create operatorsApps, first version

--- src/fr/operateurs/console/OperateursApp.java
+++ src/fr/operateurs/console/OperateursApp.java
@@ -1,7 +1,59 @@
 package fr.operateurs.console;
 
+import java.util.Scanner;
+
 public class OperateursApp {
+	private static Scanner questionUser;
+
 	public static void main(String [] args) {
-		System.out.println("Hellow World");
+		// Affichage du premier message
+		System.out.println("*****  Application Opérateurs *****\r\n" + 
+				"Veuillez saisir le premier nombre…");
+		// Récupération du premier nombre taper
+		
+		int firstNumber = getNumber();
+		// Affichage du deuxieme message
+		System.out.println("Veuillez saisir le second nombre…");
+
+		int secondNumber = getNumber();
+		showOperation(firstNumber,secondNumber,"+");
+
+		showOperation(firstNumber,secondNumber,"-");
+		showOperation(firstNumber,secondNumber,"*");
+		showOperation(firstNumber,secondNumber,"/");
+		showOperation(firstNumber,secondNumber,"%");
 	}
+	
+	private static void showOperation(int firstNumber, int secondNumber, String string) {
+
+		float result = 0 ;
+		
+		switch(string) {
+		case "+":
+			result = firstNumber + secondNumber;
+		      break;
+		case "-":
+			result = firstNumber - secondNumber;
+		      break; 
+		case "*":
+			result = firstNumber * secondNumber;
+		      break;
+		case "/":
+			result = firstNumber / secondNumber;
+		      break;
+		case "%":
+			result = firstNumber % secondNumber;
+		      break;
+		default: System.out.println("error intern, contact dev");
+		}
+		//show message
+		System.out.println(firstNumber + " " + string + " " + secondNumber + " = " + result);
+	
+	}
+
+	private static int getNumber() {
+		questionUser = new Scanner(System.in);
+		return questionUser.nextInt();
+	}
+	
 }
